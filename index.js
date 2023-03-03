d0746d0e72f231e0055c24b8f06d2f39c7710102
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-03 11:14:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,93 +2,46 @@ import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.By;
 
-public class NameLocator 
+public class Registration 
 {
-    static String page1, page2,page3,page4,page5,baseUrl;
+    String[] fName={"Rahul","Dravid","Rahul Dravid","Srinivas6"};
     static WebDriver driver;
-    
+    static String baseUrl; 
+    DriverSetup setup=new DriverSetup();
     public WebDriver setupDriver()
     {
-        driver=DriverSetup.getWebDriver();
-        return driver;
-        /*Replace this comment by the code statement to get the driver*/
         
-    }
-    public void navigate1() 
-    {
-        baseUrl="https://selenium.dev/";
+        /* Invoke the getWebDriver method 
+           Set value of BaseUrl
+           Launch the app using get() with baseUrl */
+        driver=setup.getWebDriver();
+        baseUrl="http://webapps.tekstac.com/Shopify/";
         driver.get(baseUrl);
-        /*Replace this comment by the code statement to navigate to baseUrl */
-        
+         return driver;
     }
-    public void getPageTitle1()
+    
+    public void setElements()
     {
-        page1=driver.getTitle();
-        /*Replace this comment by the code statement to get page title of  "https://selenium.dev/"*/
-        /* Assign the page title to variable 'page1' */
-        
+        /*Using the driver, Find the elements by id 
+          Set the values to the elements
+          Register the form*/
+       driver.findElement(By.id("firstname")).sendKeys(fName[0]);
+       driver.findElement(By.id("lastname")).sendKeys(fName[1]);
+       driver.findElement(By.id("username")).sendKeys(fName[2]);
+       driver.findElement(By.id("pass")).sendKeys(fName[3]);
+       driver.findElement(By.id("reg")).click();
     }
-    public void navigate2()
-    {
-        baseUrl="https://www.google.co.in";
-        driver.get(baseUrl);
-        /*Replace this comment by the code statement to navigate to https://www.google.co.in/ */
-        
-    }
-    public void getPageTitle2()
-    {
-        page2= driver.getTitle();
-        /*Replace this comment by the code statement to get page title of  https://www.google.co.in/ */
-        /* Assign the page title to variable 'page2' */
-        
-    }
-    public void navigateBack()
-    {
-        driver.navigate().back();
-        /*Replace this comment by the code statement to navigate back */
-       
-    }
-    public void getPageTitle3()
-    {
-        page3= driver.getTitle();
-        /*Replace this comment by the code statement to get page title of backed page */
-        /* Assign the page title to variable 'page3' */
-        
-    }
-    public void navigateForward()
-    {
-        driver.navigate().forward();
-        /*Replace this comment by the code statement to navigate forward */
-        
-    }
-    public void getPageTitle4()
-    {
-        page4=driver.getTitle();
-        /*Replace this comment by the code statement to get page title of the forwarded page */
-        /* Assign the page title to variable 'page4' */
-        
-    }
-    public void refreshPage()
-    {
-        driver.navigate().refresh();
-        /*Replace this comment by the code statement to refresh the page */
-       
-    }
-    public void getPageTitle5()
-    {
-        page5=driver.getTitle();
-        /*Replace this comment by the code statement to get page title of refreshed page */
-        /* Assign the page title to variable 'page5' */
-        
-    }
-
     
     public static void main(String[] args)
     {
-        NameLocator namLocator=new NameLocator();
-        namLocator.setupDriver();
-        //Implement code here
+        Registration reg=new Registration();
+        /* Implement the code here */
+       reg.setupDriver();
+       reg.setElements();
+       
     }
 
 }
+
+
 
