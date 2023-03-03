fb443a8a10be6fe2d2908132ff1b51433d3cbf3d
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-03 10:20:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-// Import stylesheets
-import './style.css';
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement; //Add required imports
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+public class PageLocator    //DO NOT Change the class Name
+{
+	String lName;
+	static WebDriver driver;
+	static WebElement element;
+	public WebDriver createDriver()  //DO NOT change the method signature
+	{
+	   driver=DriverSetup.getWebDriver();
+	   return driver;      //Invoke getWebDriver method from DriverSetup and return it
+	}
+	
+	public WebElement getPageLocator(WebDriver driver)  //DO NOT change the method signature
+	{
+	   WebElement lastName=driver.findElement(By.id("lastname"));
+	   lName = lastName.getAttribute("placeholder");
+	   return lastName;     /*Replace this comment by the code statement to get the WebElement of 'Lastname'*/
+	   /*Find the element by id */
+    
+	}
+	public String getName(WebElement element)  //DO NOT change the method signature
+	{
+	  return lName;  //Get the attribute value from the element and return it
+	}
+	
+	public static void main(String[] args){
+	    PageLocator pl=new PageLocator();
+	    String name= pl.getName(element);
+	    System.out.println("The lastName is"+ name);
+	    //Add required code
+	}
+
+}
