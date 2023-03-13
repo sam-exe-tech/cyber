8658f45ebcd1d042a06dee631fc873680f933a00
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-13 09:29:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,43 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import java.util.concurrent.TimeUnit;
 
-public class NameLocator {      //DO NOT change the class name
- 
-    public static String baseUrl; //Assign 'http://webapps.tekstac.com/Handling_Regular_Expression/' for baseUrl
-    public static WebDriver driver;
+public class NameLocator      //DO NOT Change the class name
+{
+    //Use the declared variables for stroing required values
+    static String fName;
+    static WebDriver driver;
     
-    public WebDriver createDriver()
+    public WebDriver setupDriver()           //DO NOT Change the method Signature
     {
-       return DriverSetup.getWebDriver(); //Create driver. Assign it to static variable 'driver' and return it
+        driver = DriverSetup.getWebDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        /* Replace this comment by the code statement to create and return the driver */
+        /* Naviaget to the url 'http://webapps.tekstac.com/AddressBook/'  */
+        driver.get("http://webapps.tekstac.com/AddressBook/");
+        return driver;
     }
-    
-    public void navigate(WebDriver driver){
-        baseUrl = "http://webapps.tekstac.com/Handling_Regular_Expression/"; //Navigate to the baseUrl
-        driver.get(baseUrl);
-    }
-    
-    public void setFormValues(WebDriver driver)
+    public String getNameLocator()              //DO NOT Change the method Signature
     {
-       driver.findElement(By.id("userId")).clear(); //set the value for 'Shipment for user' and submit form
-       driver.findElement(By.id("userId")).sendKeys("Shamili");
-       driver.findElement(By.id("track")).click();
-    }
-
-    public WebElement getNameResultElement(WebDriver driver) {
-       return driver.findElement(By.xpath("/html/body/div/table/tbody/tr[1]/td[2]")); //Find the element of 'Shamili' and return it
-        
-    }
-    public WebElement getShipmentResultElement(WebDriver driver) {
-         return driver.findElement(By.id("shipment"));  //Find the element of 'SHIP1236' and return it
-    }
-    public WebElement getEmailResultElement(WebDriver driver) {
-        
-       return driver.findElement(By.xpath("//div[contains(@id,\"mail\")]")); //Find the element of 'shamili93@gamil.com' and return it
+        WebElement parent = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/div/table/tbody/tr[4]/td[1]/ancestor::div"));
+       /*Using the driver, Find the element ancestor and its text and assign the text to 'fName' */
+       /*Close the driver*/
+      fName = parent.getText();
+      return fName;
     }
     
-    public boolean validateEmail(String eMailID) {
-      return eMailID.matches("\\b[A-Z0-9a-z-]+@[a-z]+\\.[a-z]{2,4}\\b"); //Validate email using regex. 
-        
-    }
-    public boolean validateShipmentId(String shipmentId) {
-       return shipmentId.matches("[A-Z0-9]{8}"); //Validate shipmentId using regex
-        
-    }    
-        
-        
-       
-  
     public static void main(String[] args)
     {
-        NameLocator reg=new NameLocator();
-         //Add required code here
-         WebDriver driver=reg.createDriver();
-         reg.navigate(driver);
-         reg.setFormValues(driver);
-         WebElement element=reg.getEmailResultElement(driver);
-         
-         System.out.println(element.getText());
-         System.out.println(driver.findElement(By.id("shipment")).getText());
-         System.out.println(driver.findElement(By.id("e- mail")).getText());
-         
-         driver.close();
+        NameLocator namLocator=new NameLocator();
+        //Add required code here
+        namLocator.setupDriver();
+        String name=namLocator.getNameLocator();
+        System.out.println("The name is "+name);
     }
 
-  
 }
+
 
 
 
