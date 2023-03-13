18e5fa263e462dad391fafac24e32af348c6986c
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-13 09:30:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,43 +2,36 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.Alert;
 
-public class NameLocator      //DO NOT Change the class name
+public class AlertTest    //DO NOT Change the class Name
 {
-    //Use the declared variables for stroing required values
-    static String fName;
-    static WebDriver driver;
+    public static WebDriver driver;
+    public WebDriver createDriver(){  //DO NOT change the method signature
     
-    public WebDriver setupDriver()           //DO NOT Change the method Signature
-    {
-        driver = DriverSetup.getWebDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        /* Replace this comment by the code statement to create and return the driver */
-        /* Naviaget to the url 'http://webapps.tekstac.com/AddressBook/'  */
-        driver.get("http://webapps.tekstac.com/AddressBook/");
-        return driver;
-    }
-    public String getNameLocator()              //DO NOT Change the method Signature
-    {
-        WebElement parent = driver.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/div/table/tbody/tr[4]/td[1]/ancestor::div"));
-       /*Using the driver, Find the element ancestor and its text and assign the text to 'fName' */
-       /*Close the driver*/
-      fName = parent.getText();
-      return fName;
+       //Implement code to create Driver from DriverSetup, assign it to 'static' variable and return it
+    driver = DriverSetup.getWebDriver();
+    return driver;
     }
     
-    public static void main(String[] args)
-    {
-        NameLocator namLocator=new NameLocator();
-        //Add required code here
-        namLocator.setupDriver();
-        String name=namLocator.getNameLocator();
-        System.out.println("The name is "+name);
+    public Alert getAlertElement(WebDriver driver){   //DO NOT change the method signature
+    
+        //Find the 'click' buttton and click it.
+        //Locate the 'Alert' element and return it
+       driver.findElement(By.xpath("/html/body/input")).click();
+       Alert alertPopUp = driver.switchTo().alert();
+       return alertPopUp;
+    }
+
+    public static void main(String[] args)  throws InterruptedException{  //DO NOT change the method signature
+          
+        AlertTest at=new AlertTest();
+        //Implement code here
+        at.createDriver();
+        System.out.println(at.getAlertElement(driver));
     }
 
 }
-
-
 
 
 
