ea5710357474dc8223343865a5c7d57d9ec0ee74
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-13 09:32:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,73 @@
-import org.openqa.selenium.WebDriver;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.Alert;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class AlertTest    //DO NOT Change the class Name
-{
+public class JavaScriptExecutor{    //Do not change the class name
+    
+    //Use the below declarations
     public static WebDriver driver;
-    public WebDriver createDriver(){  //DO NOT change the method signature
+    public static Object jsfname,jslname, jsuname, jspwd, jsphone, jsemail, jssubmit, jsreset;
     
-       //Implement code to create Driver from DriverSetup, assign it to 'static' variable and return it
-    driver = DriverSetup.getWebDriver();
-    return driver;
+    public WebDriver createDriver()
+    {
+        //Create the driver using the class DriverSetup. Assign it to the variable 'driver' and return it.
+        driver = DriverSetup.getWebDriver();
+        return driver;
     }
-    
-    public Alert getAlertElement(WebDriver driver){   //DO NOT change the method signature
-    
-        //Find the 'click' buttton and click it.
-        //Locate the 'Alert' element and return it
-       driver.findElement(By.xpath("/html/body/input")).click();
-       Alert alertPopUp = driver.switchTo().alert();
-       return alertPopUp;
-    }
-
-    public static void main(String[] args)  throws InterruptedException{  //DO NOT change the method signature
-          
-        AlertTest at=new AlertTest();
-        //Implement code here
+ 
+ 
+  public void submitForm(String fname,String lname, String uname, String pwd, String phone, String email) 
+  {
+        JavascriptExecutor jse = ((JavascriptExecutor)driver);
+        
+        //Using javascript executor, locate the elements of 'firstname','lastname','username','password'
+        //'phonenumber','email' and send the received values.
+        //Submit the form
+        jsfname = jse.executeScript("return document.getElementsByName('firstname')[0]");
+        ((WebElement)jsfname).sendKeys(fname);
+        jslname = jse.executeScript("return document.getElementsByName('lastname')[0]");
+        ((WebElement)jslname).sendKeys(lname);
+        jsuname = jse.executeScript("return document.getElementsByName('username')[0]");
+        ((WebElement)jsuname).sendKeys(uname);
+        jspwd = jse.executeScript("return document.getElementsByName('password')[0]");
+        ((WebElement)jspwd).sendKeys(pwd);
+        jsphone = jse.executeScript("return document.getElementsByName('phonenumber')[0]");
+        ((WebElement)jsphone).sendKeys(phone);
+        jsemail = jse.executeScript("return document.getElementsByName('email')[0]");
+        ((WebElement)jsemail).sendKeys(email);
+        
+        jssubmit = jse.executeScript("return document.getElementById('submit')");
+        ((WebElement)jssubmit).click();
+                
+  }
+  
+  
+  public void reset()
+  {
+      JavascriptExecutor jse = ((JavascriptExecutor)driver);
+      //Using javascript executor, locate the element reset and click it.
+      jsreset = jse.executeScript("return document.getElementById('reset')");
+      ((WebElement)jsreset).click();
+  }
+  
+  
+  public static void main(String[] args) {
+        JavaScriptExecutor at=new JavaScriptExecutor();
         at.createDriver();
-        System.out.println(at.getAlertElement(driver));
+        at.reset();
+        //Use this values to submit the form
+        at.submitForm("Rahul","Dravid","Rahul","rahul@123","6232126711","rahultest@gmail.com");
+        
+         
     }
-
+  
 }
-
 
 
