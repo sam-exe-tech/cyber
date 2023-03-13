e13efe3580b0ea59bdacaa2c0daf6c3ede7511b7
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-13 09:34:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,73 +1,132 @@
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver.Window;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.Point;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
 
-public class MultipleWindows { //Do not change the class name
+import java.io.File;
+import java.io.IOException;
+
+public class Slider{  //DO NOT Change the class Name
     
-    //Use these declarations to store respective values
-    static String parentWinHandle;
-    static String childWinHandle;
-    Set<String> winHandles;
+    static String baseUrl="http://webapps.tekstac.com/OnlineShopping/fashion.html"; 
+    public static WebDriver driver;
+    public static String productInfo;
+    public static String quantityInfo;
+    public static String priceInfo;
     
-    static WebDriver driver;
     
-    public WebDriver createDriver() //Do not change the method signature
-    {
-        driver = DriverSetup.getWebDriver(); /* Replace this comment by the code statement to create driver, assign it to 'static' variable and return the driver */
-        driver.get("https://webapps.tekstac.com/MultipleWindow/");
-        return driver;
+    public WebDriver createDriver(){       //DO NOT change the method signature
+       // Implement code to create Driver from DriverSetup and return it
+       // Get the Web Driver and assign it to the driver variable
+       // navigate to the baseUrl
+       // return driver
+       driver=DriverSetup.getWebDriver();
+       driver.get(baseUrl);
+       return driver;
     }
     
-    public String getParentWindow() throws Exception { //Do not change the method signature
-        // Get  Parent Window Handle as string and return it
-        return parentWinHandle = driver.getWindowHandle();
+    public void implDragnDrop(){   //DO NOT change the method signature
+    
+        // Locate the source web element that needs to be dragged.
+
+        // Locate the target web element that needs to be dropped.      
+
+        // Creating object of Actions class to build composite actions
+
+        // Performing the drag and drop action
+        
+        WebElement source1 = driver.findElement(By.id("Shirts"));
+        WebElement target = driver.findElement(By.id("droppable"));
+        Actions builder = new Actions(driver);
+        builder.clickAndHold(source1).moveToElement(target).release(source1).build().perform();
     }
     
-    public String  getChildWindow() throws Exception { //Do not change the method signature
+    public void implMoveSlider(){   //DO NOT change the method signature
+        // Creating object of Actions class to build composite actions
+
+        // Locate the slider web element
+
+        // Move the slider to '5'
+
+        // For Example
+
+            // You can use the below methods to move the slider
+
+            // * moveToElement(WebElement target)
+
+            // * dragAndDropBy(WebElement source, int xOffset, int yOffset)
+
+            // xOffset value is chosen as given below
+                
+                // a. If the 'kg' value is 5 the xOffset is "-10"
+                // b. If the 'kg' value is 4 the xOffset is "-20"
+
+                // c. If the 'kg' value is 3 the xOffset is "-40"
+
+                // d. If the 'kg' value is 6 the xOffset is "10"
+
+                // e. If the 'kg' value is 7 the xOffset is "20"
+
+            // yOffset value is default ‘0’
+                int slidvalue = 5; 
+                Actions builder1 = new Actions(driver);
+                WebElement slider = driver.findElement(By.id("myRange"));
+                builder1.click(slider).build().perform();
+                builder1.moveToElement(slider).click().dragAndDropBy(slider,(slidvalue * -2),0).build().perform();
+                }
+    
+    public String getProductDetail(){ //DO NOT change the method signature
+    
+        // Locate the text displaying the product name.
+        WebElement element1 = driver.findElement(By.id("product"));
+        productInfo = element1.getText();
+        System.out.println("***** product info *****");
+        System.out.println(productInfo);
+        return productInfo;
         
-        // Click the href link. Find the child window's handle and return it.
-        //Hint: WAIT for child page to load and find the child window handle.
-        WebElement newWindowBtn = driver.findElement(By.xpath("/html/body/p/a"));
-        newWindowBtn.click();
-        Thread.sleep(3000);
-        
-        winHandles = driver.getWindowHandles();
-        for(String handle: winHandles){
-            System.out.println("handle "+ handle);
-            if(!handle.equals(parentWinHandle)){
-                driver.switchTo().window(handle);
-                childWinHandle = driver.getWindowHandle();
-                Thread.sleep(2000);
-            }
-        }
-        return childWinHandle;
+        // Assign the displaying text to the static variable 'productInfo'
+
+        // Return the text using getText() method.
+
+
     }
     
     
-      
-    
-    public static void main(String[] args){   //Do not change the method signature
-        MultipleWindows win=new MultipleWindows();
-        //Implement code here
-       win.createDriver();
-       try{ 
-           System.out.println("Parent window " + win.getParentWindow());
-           System.out.println("Child window " + win.getChildWindow());
-          }catch(Exception e){
-           e.printStackTrace();
-          }
-        
+    public String getQuantityDetail(){ //DO NOT change the method signature
+
+        // Locate the text displaying the product quantity(No). 
+        WebElement element2 = driver.findElement(By.xpath("//html/body/div[4]/div/div/p[3]"));
+        // Assign the displaying text to the static variable 'quantityInfo'
+        quantityInfo=element2.getText();
+        System.out.println("***** Quantity info *****");
+        System.out.println(quantityInfo);
+        // Return the text using getText() method.
+        return quantityInfo;
+    }
+   
+   
+    public String getPriceDetail(){  //DO NOT change the method signature
+        // Locate the text displaying the product price. 
+           WebElement element2 = driver.findElement(By.id("price"));
+        // Assign the displaying text to the static variable 'priceInfo'
+           priceInfo = element2.getText();
+           System.out.println("***** price info *****");
+           System.out.println(priceInfo);
+        // Return the text using getText() method.
+        return priceInfo;
+    }
+
+    public static void main(String[] args){          //DO NOT change the method signature
+        Slider ex =new Slider();
+        ex.createDriver();
+        ex.implDragnDrop();
+        ex.implMoveSlider();
+        ex.getPriceDetail();
+        ex.getQuantityDetail();
+        ex.getPriceDetail();
+        driver.close();
+        //Implement the required code
+        //Close the driver
     }
 }
-
-
