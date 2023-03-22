e99d6e10a752dfe8a386e397b277ee3b1ee7a996
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-22 23:02:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,163 +18,104 @@ import javax.xml.xpath.XPathFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
-public class CommodityDetails {        //Do not change the class name
+
+
+public class DiscountCalculator{      //Do not change the class name
     
-    //Use the below declarations    
+    //use this variable declaration
     public static WebDriver driver;
-    public static XPath xPath;
-    public static Document doc;  
+    public XPath xpath;
+    public Document doc;
     
-    public static String name,weight,length,width,height;
     
-    public static WebDriver createDriver(){     //Do not change the method signature
-       //Create the driver using the class 'DriverSetup'. Assign it to the variable 'driver' and return it.
-       //Use URL, "http://webapps.tekstac.com/CommodityDetails/"
-       driver=DriverSetup.getWebDriver();
-       driver.get("http://webapps.tekstac.com/CommodityDetails/");
-       return driver;
-       
-    }
+    public static WebDriver createDriver(){   //Do not change the method signature
     
-    public static XPath readFile(String fileName,String id){     //Do not change the method signature
-        //Pass fileName and commodity id as parameter
-        //Parse the xml file. Return the xpath reference
-        try {
-            DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
-            DocumentBuilder builder=factory.newDocumentBuilder();
-            doc=builder.parse(fileName);
-            
-            
-        } catch(Exception e) 
-        {
-            System.out.println(e);
-        } 
-        doc.getDocumentElement().normalize();
-        XPathFactory xPathFactory=XPathFactory.newInstance();
-        xPath=xPathFactory.newXPath();
-        
-        return xPath;
+        /* Create a driver. Assign it to static variable 'driver' and return it */
+        /* navigate to 'https://webapps.tekstac.com/CompanyOffersDiscount/'  */
+        driver=DriverSetup.getWebDriver();
+        driver.get("https://webapps.tekstac.com/CompanyOffersDiscount/");
+        return driver;
         
     }
     
-    public static Node getName(int id){       //Do not change the method signature
-        //Pass the commodity id as parameter. Parse name and return it node.
-        Node node=null;
+    public XPath ReadFile(String fileName,String id){      //Do not change the method signature
+        //Retrieve the xml file name passed as 'fileName' parameter. Parse the xml and return the xPath
+        //Parameter 'id' is the id in the DiscountCalculator.xml
         try{
-            node=(Node)xPath.compile("//Commodity[@id="+id+"]/Name").evaluate(doc,XPathConstants.NODE);
+            String fname,lname,uname,pass=null;
+            String projectPath=System.getProperty("user.dir");
+            System.out.println(projectPath);
+            File fxmlFile=new File(projectPath+File.separator+fileName);
+            System.out.println(projectPath+File.separator+fileName);
+            DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder=null;
+            builder=factory.newDocumentBuilder();
+            doc=builder.parse(fxmlFile);
+            XPathFactory xPathFactory=XPathFactory.newInstance();
+            xpath=xPathFactory.newInstance().newXPath();
             
         }catch(Exception e)
         {
-            
+            e.printStackTrace();
         }
-        return node;
+        return xpath;
     }
     
-    public  static Node getWeight(int id){     //Do not change the method signature
-        //Pass the commodity id as parameter. Parse weight and return it node.
-        Node node=null;
-        try {
-            node=(Node)xPath.compile("//Commodity[@id="+id+"]/Weight").evaluate(doc,XPathConstants.NODE);
+    public  Node getWeight(int id){   //Do not change the method signature
+        //Parse the xml to get 'Weight' element. Return its node
+        //Parameter 'id' is the id in the DiscountCalculator.xml
+        String UweightEx=String.format("/Shipmentdetails/Shipmentdetail[@id='"+id+"']/Weight");
+        Node node1=null;
+        try{
+            node1=(Node)xpath.compile(UweightEx).evaluate(doc,XPathConstants.NODE);
             
-        } catch(Exception e) 
+        }catch (Exception e)
         {
-            
         }
-        return node;
+        String uweight=node1!=null?(node1.getTextContent()):"cannot read the test case xml file";
+        return node1;
     }
     
-    public  static Node getLength(int id){   //Do not change the method signature
-        //Pass the commodity id as parameter. Parse length and return it node.
-        Node node=null;
-        try {
-            node=(Node)xPath.compile("//Commodity[@id="+id+"]/Length").evaluate(doc,XPathConstants.NODE);
-            
-        } catch(Exception e) 
-        {
-            
-        }
-        return node;
-    }
-    
-    public static Node getWidth(int id){      //Do not change the method signature
-       //Pass the commodity id as parameter. Parse width and return it node.
-       Node node=null;
-       try {
-           node=(Node)xPath.compile("//Commodity[@id="+id+"]/Width").evaluate(doc,XPathConstants.NODE);
-           
-       } catch(Exception e) 
-       {
-           
-       }
-       return node;
-    }
-    
-    
-    public static Node getHeight(int id){     //Do not change the method signature
-        //Pass the commodity id as parameter. Parse heigth and return it node.
-        Node node=null;
-        try {
-            node=(Node)xPath.compile("//Commodity[@id="+id+"]/Height").evaluate(doc,XPathConstants.NODE);
-            
-        } catch(Exception e) 
-        {
-            
-        }
-        return node;
-    }
- 
-    public static void readInsertedData(){     //Do not change the method signature
-        //Locate the row displayed on the page after submit.
-        //Find the text of of name,weight,height,width and length and store it in respective variables declared above
-        name=driver.findElement(By.xpath("//table[@id='myTable']/tbody/tr[2]/td[1]")).getText();
-        weight=driver.findElement(By.xpath("//table[@id='myTable']/tbody/tr[2]/td[2]")).getText();
-        length=driver.findElement(By.xpath("//table[@id='myTable']/tbody/tr[2]/td[3]")).getText();
-        width=driver.findElement(By.xpath("//table[@id='myTable']/tbody/tr[2]/td[4]")).getText();
-        height=driver.findElement(By.xpath("//table[@id='myTable']/tbody/tr[2]/td[5]")).getText();
-    }
-    
-    
-    
-    public static String getCommodityCount(){    //Do not change the method signature
-        //Locate commodity count value after submit and return it
-        String count=driver.findElement(By.id("count")).getAttribute("value");
-        return count;
-    }
-    
-    public static String getTotalComWeight(){     //Do not change the method signature
-        //Locate total weight value after submit and return it
-        String total=driver.findElement(By.id("total")).getAttribute("value");
-        return total;
-    }
-     
-    
-      public   static void  submitForm(int id){   //Do not change the method signature
-        //Locate the form elements and send the values parsed from xml.
-        //Submit the form.
-        //Pass commodity id as parameter
-        driver.findElement(By.id("name")).sendKeys(getName(id).getTextContent());
-        driver.findElement(By.id("weight")).sendKeys(getWeight(id).getTextContent());
-        driver.findElement(By.id("length")).sendKeys(getLength(id).getTextContent());
-        driver.findElement(By.id("width")).sendKeys(getWidth(id).getTextContent());
-        driver.findElement(By.id("height")).sendKeys(getHeight(id).getTextContent());
-        driver.findElement(By.id("add")).click();
+    public  Node getDistance(int id){   //Do not change the method signature
+        //Parse the xml to get 'Distance' element. Return its node
+        //Parameter 'id' is the id in the DiscountCalculator.xml
+        String UdistanceEx=String.format("/Shipmentdetails/Shipmentdetail[@id='"+id+"']/Distance");
+        Node node1=null;
+        try{
+            node1=(Node)xpath.compile(UdistanceEx).evaluate(doc,XPathConstants.NODE);
         
-      }
+            
+        }catch(Exception e)
+        {
+        }
+        String udistance=node1!=null?(node1.getTextContent()):"cannot read the test case xml file";
+        return node1;
+    }
+    
+    
+    
+    public  String  getMessage(){  //Do not change the method signature
+        
+        //Find the web elements in the page. Assign the respective values from xml to the form.
+        //Submit the form 
+        //Locate the 'Datax shipping company offers discount' message and return it
+        System.out.println("Inside Message");
+        driver.findElement(By.id("weight")).sendKeys(getWeight(1).getTextContent());
+        driver.findElement(By.id("distance")).sendKeys(getDistance(1).getTextContent());
+        driver.findElement(By.id("submit")).click();
+        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+        String message=driver.findElement(By.xpath("//*[@id=\"result\"]")).getText();
+        System.out.println("Message: "+message);
+        return message;
+        
+     }
 
-    public static void main(String[] args)
-    {
-        CommodityDetails commodity=new CommodityDetails();
-        //Implement code here
-        createDriver();
-        readFile("commodity.xml","1");
-        submitForm(1);
-        readInsertedData();
-        getCommodityCount();
-        getTotalComWeight();
+    public static void main(String[] args){
+        DiscountCalculator pagLocator=new DiscountCalculator();
+        WebDriver driver=pagLocator.createDriver();
+        pagLocator.ReadFile("DiscountCalculator.xml","1");
+        String message=pagLocator.getMessage();
         driver.close();
+        //Add required code here
     } 
-       
-
-        
-
-    }
+}
