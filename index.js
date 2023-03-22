8c280d80a07d6d0a210447c4dd419e968f498b34
sam-exe-tech <100674375+sam-exe-tech@users.noreply.github.com>
2023-03-22 22:58:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,164 +3,217 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.By;
 
-import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Map;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 
-public class UserDetails {
 
-    //use the below variable declarations
-    public static WebDriver driver;
-    public static JSONArray details;
+
+public class UserDetails{      //Do not change the class name
     
-    public static WebDriver createDriver() {    //Do not change the class name
-        driver=DriverSetup.getWebDriver();
-        driver.get("http://webapps.tekstac.com/FormRegistration/UserRegistration.html");
-        return driver;
+    //use this variable declaration
+    public static WebDriver driver;
+    public static Document doc;
+    public static XPath xpath;
+    
+    public static WebDriver createDriver(){   //Do not change the method signature
+    
         /* Create a driver. Assign it to static variable 'driver' and return it */
         /* navigate to 'http://webapps.tekstac.com/FormRegistration/UserRegistration.html'  */
+        
+        driver=DriverSetup.getWebDriver();
+        driver.get("http://webapps.tekstac.com/FormRegistration/UserRegistration.html");
+        
+        return driver;
     }
     
-    public JSONArray readFile(String fileName) {   //Do not change the method signature
-       //Retrieve the json file name passed as 'fileName' parameter. Parse the json, store it in 'details' array and return it
-       try{
-           JSONParser parser=new JSONParser();
-           FileReader reader=new FileReader(fileName);
-           Object obj=parser.parse(reader);
-           JSONObject jobj=(JSONObject)obj;
-           details=(JSONArray)jobj.get("Userdetails");
-       }catch(Exception e)
-       {
-    }finally{
+    public XPath ReadFile(String fileName,String id){      //Do not change the method signature
+        //Retrieve the xml file name passed as 'fileName' parameter. Parse the xml and return the xPath
+        //Parameter 'id' is the id in the Userdetails.xml
+        try{
+            DocumentBuilderFactory factory=DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder=factory.newDocumentBuilder();
+            doc=builder.parse(fileName);
+        } catch(Exception e){
+           System.out.println(e); 
         }
-        return details;
+        doc.getDocumentElement().normalize();
+        XPathFactory xPathFactory=XPathFactory.newInstance();
+        xpath=xPathFactory.newXPath();
+        return xpath;
     }
-
-    public String getName(int id) {   //Do not change the method signature
-        //Parse the json to get 'Name' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject name=(JSONObject)details.get(id);
-        String nname=(String)name.get("Name");
-        return nname;
+    
+    public  Node getName(int id){   //Do not change the method signature
+        //Parse the xml to get 'Name' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Name").evaluate(doc,XPathConstants.NODE);
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
     }
-
-    public String getEmail(int id) {   //Do not change the method signature
-        //Parse the json to get 'Email' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject email=(JSONObject)details.get(id);
-        String eemail=(String)email.get("Email");
-        return eemail;
+    
+    public  Node getEmail(int id){   //Do not change the method signature
+        //Parse the xml to get 'Email' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Email").evaluate(doc,XPathConstants.NODE);
+        
+            }catch(Exception e)
+            {
+                System.out.println(e);
+            }
+            return node;
     }
-
+    
+    public  Node getPhone(int id){    //Do not change the method signature
+        //Parse the xml to get 'Phone' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Phone").evaluate(doc,XPathConstants.NODE);
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
+    }
+    
+    public  Node getAge(int id){        //Do not change the method signature
+        //Parse the xml to get 'Age' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Age").evaluate(doc,XPathConstants.NODE);
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
+    }
     
     
-    public String getPhone(int id) {     //Do not change the method signature   
-        //Parse the json to get 'Phone' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject phone=(JSONObject)details.get(id);
-        String pphone=(String)phone.get("Phone");
-        return pphone;
+    public  Node getPassword(int id){    //Do not change the method signature
+        //Parse the xml to get 'Password' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Password").evaluate(doc,XPathConstants.NODE);
+            
+        } catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
     }
-
     
-    public String getAge(int id) {         //Do not change the method signature
-        JSONObject age=(JSONObject)details.get(id);
-        String aage=(String)age.get("Age");
-        return aage;
-        //Parse the json to get 'Age' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-    }
-
     
-    public String getPassword(int id) {      //Do not change the method signature
-    //Parse the json to get 'Password' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject pass=(JSONObject)details.get(id);
-        String ppass=(String)pass.get("Password");
-        return ppass;
-    }
-
     
-    public String getHobbies(int id) {        //Do not change the method signature
-        //Parse the json to get 'Hobbies' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject hobbies=(JSONObject)details.get(id);
-        String hhobbies=(String)hobbies.get("Hobbies");
-        return hhobbies;
+    public  Node getHobbies(int id){    //Do not change the method signature
+        //Parse the xml to get 'Hobbies' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Hobbies").evaluate(doc,XPathConstants.NODE);
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
     }
-
     
-    public String getGender(int id) {          //Do not change the method signature
-        //Parse the json to get 'Gender' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject gender=(JSONObject)details.get(id);
-        String ggender=(String)gender.get("Gender");
-        return ggender;
+    
+    public  Node getGender(int id){    //Do not change the method signature
+        //Parse the xml to get 'Gender' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Gender").evaluate(doc,XPathConstants.NODE);
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
     }
-
-    public String getCity(int id) {          //Do not change the method signature
-        //Parse the json to get 'City' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject city=(JSONObject)details.get(id);
-        String ccity=(String)city.get("City");
-        return ccity;
+    
+    
+    public  Node getCity(int id){        //Do not change the method signature
+        //Parse the xml to get 'City' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/City").evaluate(doc,XPathConstants.NODE);
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
     }
-
-    public String getAddress(int id) {         //Do not change the method signature
-        //Parse the json to get 'Address' value. Return it.
-        //Parameter 'id' is the id in the Registration.json
-        JSONObject address=(JSONObject)details.get(id);
-        String aaddress=(String)address.get("Address");
-        return aaddress;
+    
+    
+    public  Node getAddress(int id){       //Do not change the method signature
+       //Parse the xml to get 'Address' element. Return its node
+        //Parameter 'id' is the id in the Userdetails.xml
+        Node node=null;
+        try{
+            node=(Node)xpath.compile("//User[@id="+id+"]/Address").evaluate(doc,XPathConstants.NODE);
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return node;
     }
-
-    public String getMessage() {           //Do not change the method signature
-        //Find the web elements in the page. Assign the respective values from json to the form.
+    
+    
+      public  String  getMessage(){  //Do not change the method signature
+        
+        //Find the web elements in the page. Assign the respective values from xml to the form.
         //Submit the form 
         //Locate the 'Registered Successfully' message and return it
-        Select cCity=new Select(driver.findElement(By.id("city")));
-        driver.findElement(By.xpath("//input[@type='reset']")).click();
-        driver.findElement(By.id("uname")).sendKeys(getName(1));
-        driver.findElement(By.id("uemail")).sendKeys(getEmail(1));
-        driver.findElement(By.id("phone")).sendKeys(getPhone(1));
-        driver.findElement(By.id("age")).sendKeys(getAge(1));
-        driver.findElement(By.id("pass")).sendKeys(getPassword(1));
-        if(getHobbies(1).equals("Cricket"))
-        {
-            driver.findElement(By.id("option1")).click();
-            
-        }
-        else 
-        {
-            driver.findElement(By.id("option2")).click();
-        }
-        if(getGender(1).equals("Male"))
-        {
-            driver.findElement(By.xpath("//input[@id='male']/parent::label")).click();
-        }
-        else{
-            driver.findElement(By.xpath("//input[@id='female']/parent::label")).click();
-        }
-        cCity.selectByVisibleText(getCity(1));
-        driver.findElement(By.name("address")).sendKeys(getAddress(1));
+        driver.findElement(By.id("uname")).sendKeys(getName(1).getTextContent());
+        driver.findElement(By.id("uemail")).sendKeys(getEmail(1).getTextContent());
+        driver.findElement(By.id("phone")).sendKeys(getPhone(1).getTextContent());
+        driver.findElement(By.id("age")).sendKeys(getAge(1).getTextContent());
+        driver.findElement(By.id("pass")).sendKeys(getPassword(1).getTextContent());
+        driver.findElement(By.xpath("//input[@id='option1']/parent::label")).click();
+        driver.findElement(By.xpath("//input[@id='male']/parent::label")).click();
+        Select city=new Select(driver.findElement(By.id("city")));
+        city.selectByVisibleText(getCity(1).getTextContent());
+        driver.findElement(By.name("address")).sendKeys(getAddress(1).getTextContent());
         driver.findElement(By.id("submit")).click();
-        String msg=driver.findElement(By.xpath("/html/body/h2")).getText();
+        String msg=driver.findElement(By.tagName("h2")).getText();
         return msg;
-    }
+        
+      }
 
-    public static void main(String[] args) {
-        UserDetails fieldLocator = new UserDetails();
-        //Add required code
+    public static void main(String[] args){
+        UserDetails pagLocator=new UserDetails();
         createDriver();
-        fieldLocator.readFile("Registration.json");
-        String str=fieldLocator.getMessage();
-        System.out.println(str);
+        pagLocator.ReadFile("UserDetails.xml","1");
+        System.out.println(pagLocator.getMessage());
         driver.close();
-    }
-
+        //Add required code here
+    } 
 }
